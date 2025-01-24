f9c5cbf01e9c042c3492efc2fedb433eb730036a
Nisarg.A <nisarjapure@gmail.com>
2025-01-24 11:50:16 +0530

    nodehealth check script

---
+++ nodehealth.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+###############
+# Author: Nisarg
+# Date: 24/01/2025
+#
+# This script output the node health
+#
+# Version: v1
+# #############
+
+set -x #Debug mode
+
+df -h #for printing disk space
+
+
+free -g #for printing the memory
+
+
+nproc  #printing the resources
+
